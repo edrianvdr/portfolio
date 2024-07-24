faa39979797b4c7acdb3d038c9b22a41db8d1203
edrianvdr <edrianvdr@gmail.com>
2024-07-25 00:23:53 +0800

    Fixing for Netlify

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './index.css';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import About from './components/About';
 import Projects from './components/sections/Projects';
 import Skills from './components/sections/Skills';
 import WorkExperience from './components/sections/WorkExperience';
