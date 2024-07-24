68a062a3041fb0c4f4b51d9801996d6298bfb097
edrianvdr <edrianvdr@gmail.com>
2024-07-24 20:40:26 +0800

    Added resume function, fixed Project component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,20 @@ import Navigation from './Navigation';
 import edrianImage from '../assets/images/EVDR 02.png';
 
 function Header() {
+    const handleResumeClick = () => {
+        window.open(`${process.env.PUBLIC_URL}/documents/resume.pdf`, '_blank');
+    };
+
     return (
         <header className="bg-blue-800 text-white mb-4">
             <div className="container mx-auto grid grid-cols-1 md:grid-cols-12 text-center">
                 <div className="sm:col-span-7 flex flex-col justify-center items-center xl:pl-96">
                     <h1 className="text-3xl sm:text-4xl lg:text-5xl font-serif font-bold mt-4 mb-2">Edrian Delos Reyes</h1>
                     <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl mb-4">Web Developer</p>
-                    <button className="bg-white text-blue-800 border border-blue-800 hover:bg-blue-800 hover:text-white py-2 px-6 rounded-full text-lg font-semibold shadow-md transition duration-300">
+                    <button 
+                        className="bg-white text-blue-800 border border-blue-800 hover:bg-blue-800 hover:text-white hover:border-white focus:bg-blue-800 focus:text-white focus:outline-none focus:border-white py-2 px-6 rounded-full text-lg font-semibold shadow-md transition duration-300"
+                        onClick={handleResumeClick}
+                    >
                         Resume
                     </button>
                     {/* <Navigation /> */}
--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -83,7 +83,7 @@ const Awards = () => {
             onClick={loadMore}
             className="inline-flex items-center px-8 py-4 bg-blue-800 text-white text-4xl font-bold rounded-full border hover:bg-white hover:text-blue-800 hover:border-blue-800 focus:outline-none focus:bg-white focus:text-blue-800 focus:border-blue-800 transition duration:300"
           >
-            See More
+            See More <span className="sr-only">Projects</span>
           </button>
         </div>
       )}
