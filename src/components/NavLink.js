efaf75de90420e29e3d8bf92cf84b5cb83e0ba43
edrianvdr <edrianvdr@gmail.com>
2024-07-31 00:16:30 +0800

    Removed accessKeys

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function NavLink({ href, accesskey, value }) {
+function NavLink({ href, value }) {
   return (
     <a 
       href={href}
       className="border-b hover:bg-white hover:text-blue-800 focus:bg-white focus:text-blue-800 focus:outline-none focus-visible:ring-2 focus-visible:ring-white hover:rounded focus:rounded py-2 px-4 mx-2 transition duration-300"
-      accessKey={accesskey}
     >
       {value}
     </a>
@@ -16,8 +15,6 @@ function NavLink({ href, accesskey, value }) {
 NavLink.propTypes = {
   href: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
-  accesskey: PropTypes.string,
-
 };
 
 export default NavLink;
--- src/components/sections/Navigation.js
+++ src/components/sections/Navigation.js
@@ -6,13 +6,13 @@ function Navigation() {
     <nav className="w-full bg-blue-800 text-white">
       <div className="container mx-auto">
         <div className="flex md:flex-row flex-col sm:items-center justify-center text-sm font-bold mt-0 px-44 py-2">
-          <NavLink href="Services" accesskey="s" value="Services" />
+          <NavLink href="Services" value="Services" />
           <NavLink href="WorkExperience" value="Work Experience"/>
-          <NavLink href="#Projects" accesskey="p" value="Projects" />
+          <NavLink href="#Projects" value="Projects" />
           <NavLink href="#Education" value="Education"/>
           <NavLink href="#Skills" value="Skills"/>
           <NavLink href="#Awards" value="Awards"/>
-          <NavLink href="#ContactMe" accesskey="c" value="Contact Me"/>
+          <NavLink href="#ContactMe" value="Contact Me"/>
         </div>
       </div>
     </nav>
