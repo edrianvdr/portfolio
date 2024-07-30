81e5fa4f1f0ee2f8eebc0d36679a32c5a012193f
edrianvdr <edrianvdr@gmail.com>
2024-07-31 00:19:02 +0800

    Fixed services and workexperience navlinks

--- src/components/sections/Navigation.js
+++ src/components/sections/Navigation.js
@@ -6,8 +6,8 @@ function Navigation() {
     <nav className="w-full bg-blue-800 text-white">
       <div className="container mx-auto">
         <div className="flex md:flex-row flex-col sm:items-center justify-center text-sm font-bold mt-0 px-44 py-2">
-          <NavLink href="Services" value="Services" />
-          <NavLink href="WorkExperience" value="Work Experience"/>
+          <NavLink href="#Services" value="Services" />
+          <NavLink href="#WorkExperience" value="Work Experience"/>
           <NavLink href="#Projects" value="Projects" />
           <NavLink href="#Education" value="Education"/>
           <NavLink href="#Skills" value="Skills"/>
